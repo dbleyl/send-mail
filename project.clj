1bc18cb01302febf20570692d177a341e042458d
Donald Bleyl <donald.bleyl@gmail.com>
2011-12-23 07:51:47 -0500

    added lein-clojars dep and changed version number to 3-digit.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject send-mail "0.1"
+(defproject send-mail "0.1.0"
             :description "A Clojure library for sending email through gmail."
             :url "http://github.com/dbleyl/send-mail"
-            :dependencies [ [org.clojure/clojure "1.3.0"] ])
+            :dependencies [ [org.clojure/clojure "1.3.0"] ]
+            :dev-dependencies [ [lein-clojars "0.7.0"] ])
