1f8d85c88c1e696564cf114305686258626a0e74
Donald Bleyl <donald.bleyl@gmail.com>
2011-12-22 21:11:36 -0500

    added project.clj file.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject send-mail "0.1"
+            :description "A Clojure library for sending email through gmail."
+            :url "http://github.com/dbleyl/send-mail"
+            :dependencies [ [org.clojure/clojure "1.3.0"] ])
